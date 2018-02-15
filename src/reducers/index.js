f0216b1274fee2fc1fffce224d6c05cd371db89c
Jing Chai <jingweichai@gmail.com>
2018-02-15 16:39:00 +0800

    Add redux reducers, utilizing reducer composition to break up substates.

---
+++ src/reducers/index.js
@@ -0,0 +1,102 @@
+import { combineReducers } from 'redux';
+
+import {
+  ACTIVE_CARD_SELECT,
+  ACTIVE_CARD_FLIP,
+  CARD_ADD,
+  CARD_DELETE,
+  CARD_EDIT,
+} from './../actions';
+
+/*
+ * Shape of State object
+ */
+/*
+{
+  cards: {id: int, word: string, definition: string},
+  activeCard: {id: int, isFlipped: boolean},
+}
+*/
+
+const initialState = {
+  cards: [
+    {
+      id: 1,
+      word: 'mouse',
+      definition: 'A small mammal.',
+    },
+  ],
+  activeCard: {
+    id: 1,
+    isFlipped: false,
+  },
+};
+
+/**
+ * Active card reducer
+ */
+const activeCard = (state = initialState.activeCard, action) => {
+  switch (action.type) {
+    case ACTIVE_CARD_SELECT:
+      return {
+        ...state,
+        id: action.id,
+        isFlipped: false,
+      };
+
+    case ACTIVE_CARD_FLIP:
+      return {
+        ...state,
+        isFlipped: true,
+      };
+
+    default:
+      return state;
+  }
+};
+
+/**
+ * Cards reducer
+ */
+const cards = (state = initialState.cards, action) => {
+  switch (action.type) {
+    case CARD_ADD:
+      return [
+        ...state,
+        {
+          id: state.cards.length,
+          word: action.word,
+          definition: action.definition,
+        },
+      ];
+
+    case CARD_DELETE:
+      return state.cards.filter(c => c.id !== action.id);
+
+    case CARD_EDIT:
+      return state.cards.map(c => (c.id === action.id ? {
+        id: action.id,
+        word: action.word,
+        definition: action.definition,
+      } : c));
+
+    default:
+      return state;
+  }
+};
+
+/**
+ * Compose reducers to main reducer
+ */
+// Can reduce composition code by using `combineReducers` from 'redux' npm module.
+// export const appReducer = (state = initialState, action) => ({
+//   activeCard: activeCard(state.activeCard, action),
+//   cards: cards(state.cards, action),
+// });
+
+const appReducer = combineReducers({
+  activeCard,
+  cards,
+});
+
+export default appReducer;
