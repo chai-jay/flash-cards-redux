2aba4920b02db11986319e9988d840c880f45a0c
Jing Chai <jingweichai@gmail.com>
2018-02-15 16:36:59 +0800

    Add redux action creators.

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,34 @@ export const ACTIVE_CARD_FLIP = 'ACTIVE_CARD_FLIP';
 export const CARD_ADD = 'CARD_ADD';
 export const CARD_DELETE = 'CARD_DELETE';
 export const CARD_EDIT = 'CARD_EDIT';
+
+/**
+ * Action Creators
+ */
+export const activeCardSelect = ({ id }) => ({
+  type: ACTIVE_CARD_SELECT,
+  id,
+});
+
+export const activeCardFlip = ({ id }) => ({
+  type: ACTIVE_CARD_FLIP,
+  id,
+});
+
+export const cardAdd = ({ word, definition }) => ({
+  type: CARD_ADD,
+  word,
+  definition,
+});
+
+export const cardDelete = ({ id }) => ({
+  type: CARD_DELETE,
+  id,
+});
+
+export const cardEdit = ({ id, word, definition }) => ({
+  type: CARD_EDIT,
+  id,
+  word,
+  definition,
+});
