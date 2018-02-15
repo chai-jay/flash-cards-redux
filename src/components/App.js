4e9531dfa9e2e1799bf06cae8304b629cd66d9a0
Jing Chai <jingweichai@gmail.com>
2018-02-15 16:33:17 +0800

    Add initial project files from create-react-app with linting, redux dependencies, and Bootstrap CSS.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Nav from './nav/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        App code
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => (
+  <header>
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <a className="navbar-brand" href="/">Flash Cards</a>
+      {/* <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" activeClassName="active" exact to="/">
+            Home
+            <span className="sr-only">(current)</span>
+          </a>
+        </div>
+      </div> */}
+    </nav>
+  </header>
+);
+
+export default Nav;
