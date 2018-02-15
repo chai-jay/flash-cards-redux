363603cfa9646869017e07f41d9c9c7a78d6a042
Jing Chai <jingweichai@gmail.com>
2018-02-15 16:36:31 +0800

    Add redux action types.

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+/**
+ * Action Types
+ */
+// For Active Card substate
+export const ACTIVE_CARD_SELECT = 'ACTIVE_CARD_SELECT';
+export const ACTIVE_CARD_FLIP = 'ACTIVE_CARD_FLIP';
+// For Cards substate
+export const CARD_ADD = 'CARD_ADD';
+export const CARD_DELETE = 'CARD_DELETE';
+export const CARD_EDIT = 'CARD_EDIT';
